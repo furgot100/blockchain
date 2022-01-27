d2fd9dce475b941032f0ee12834ad2a5b5018211
Francis Tsang <frtsang40@gmail.com>
2022-01-27 11:59:17 -0800

    setting up methods

--- blockchain.py
+++ blockchain.py
@@ -3,30 +3,61 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
 
-        #This function creates new blocks and then adds to the existing chain
+        block = {
 
-        pass
+            'index': len(self.chain) + 1,
+
+            'timestamp' : time(),
+
+            'proof': proof,
+
+            previous_hash: previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions=[]
+
+        self.chain.append(block)
+
+        return block
 
     def new_transaction(self):
+        """This will create a new transaction which will be sent to the next block. It will contain
 
-        #This function adds a new transaction to already existing transactions
+        three variables including sender, recipient and amount
 
-        pass
+        """
 
-    @staticmethod
+        self.current_transactions.append(
+            {
+                'sender': sender,
+
+                'recipient': recipient,
+
+                'amount': amount,
+            }
+        )
+
+
+        return self.last_block['index'] + 1
+
+    @staticmethod   
     def hash(block):
 
-       #Used for hashing a block
-       pass
+        """The follow code will create a SHA-256 block hash and also ensure that the dictionary is ordered"""
+
+        block_string = json.dumps(block, sort_keys=True).encode()
+
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
 
        # Calls and returns the last block of the chain
 
-       pass
+       return self.chain[-1]
 
 
